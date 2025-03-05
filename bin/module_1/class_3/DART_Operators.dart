26bcf1de3e62f29fa40b9b0d7d1f3a9eacefaf5b
taufiqurSabbir <taufiqur.sabbir11@gmail.com>
2025-03-05 23:55:23 +0600

    module_1 class 3

---
+++ bin/module_1/class_3/DART_Operators.dart
@@ -0,0 +1,30 @@
+main(){
+
+  //Assignment Operators
+
+  int a = 5;
+
+  a+=3; // a= a+3
+  print(a);
+
+  a-=2; // a= a-2
+  print(a);
+
+  a*=2; // a= a*2
+  print(a);
+
+  //Relational Operators
+  int x = 10, y = 20;
+
+  print(x<y);
+  print(x>y);
+  print(x==y);
+  print(x!=y);
+
+  //logical Operator
+
+  print((x > 5) && (y>20)); //and
+  print((x >= 5) && (y>=20)); //and
+  print((x > 5) || (y>20)); //Or
+  print(!(x==y)); //not equal
+}
---
+++ bin/module_1/class_3/final_const.dart
@@ -0,0 +1,14 @@
+main(){
+  final DateTime nowDateTime; //runtime time
+  const double pi = 3.1416; // compile  time;
+
+  nowDateTime = DateTime.now();
+
+  print(nowDateTime);
+
+
+
+
+
+
+}
