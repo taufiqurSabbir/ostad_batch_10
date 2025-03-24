11e7d03fd6effbed22daeb8cc4dc92901ca2aee1
Taufiqur Rahman <62732164+taufiqurSabbir@users.noreply.github.com>
2025-03-24 23:28:05 +0600

    module-4 class_2

--- bin/Module_4/inheritence.dart
+++ bin/Module_4/inheritence.dart
@@ -1,7 +1,33 @@
-import 'father.dart';
+
+class Father {
+  String name;
+  String land = ' 100 bigha';
+  String hou = "Tin basa";
+
+
+
+  Father(this.name){
+    print("$name object created");
+  }
+
+  incomeSource(){
+    print('Farming');
+  }
+
+}
 
 class Son extends Father{
   String sonName;
+
+  @override
+  incomeSource() {
+    print('App developer');
+  }
+
+
+  getFatherIncomeSource(){
+    super.incomeSource();
+  }
   
   Son(this.sonName,String fatherName) : super(fatherName);
 
@@ -11,6 +37,8 @@ main(){
   Son Rifat = Son('Rifat','Rahim');
   Rifat.land = '150 bigha';
   Rifat.hou = 'Shad er basa';
+  Rifat.incomeSource();
+  Rifat.getFatherIncomeSource();
 
 
   print(Rifat.land);
---
+++ bin/Module_4/inheritence_2.dart
@@ -0,0 +1,55 @@
+abstract class Animal{
+  String name;
+  static var test = 'This is animal class';
+  Animal(this.name);
+
+  eat(){
+    print("$name is eating");
+  }
+
+  speak(){
+    print("$name is speaking");
+  }
+}
+
+
+class Dog extends Animal{
+  @override
+  speak(){
+    print('Ghew Ghew');
+  }
+  String color;
+  Dog(this.color,String name) : super(name);
+}
+
+
+class Cat extends Animal{
+  @override
+  speak(){
+    print('Mew Mew');
+  }
+  String color;
+  Cat(this.color,String name) : super(name);
+}
+
+
+main(){
+
+  // static use
+
+  print(Animal.test);
+
+  Dog tom = Dog('Black', 'Tom');
+
+  tom.speak();
+
+
+Cat ABC = Cat('white', 'ABC');
+  ABC.eat();
+  ABC.speak();
+
+
+
+
+
+}
